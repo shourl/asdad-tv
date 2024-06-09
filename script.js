2488c2e259dfdcd494314585bf890793031097f0
shourl <123009101+shourl@users.noreply.github.com>
2024-06-09 14:28:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,40 @@ document.addEventListener('DOMContentLoaded', function () {
   const player = new Plyr('#player', {});
 
   const channelList = document.getElementById('channelList');
+  const searchInput = document.getElementById('searchInput');
   const channels = [
-    { name: 'Channel 1', url: 'URL_DO_M3U8_1' },
-    { name: 'Channel 2', url: 'URL_DO_M3U8_2' },
+    { name: 'Channel 1', url: 'https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8' },
+    { name: 'Channel 2', url: 'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8' },
     // Dodaj więcej kanałów według potrzeb
   ];
 
-  channels.forEach(channel => {
-    const listItem = document.createElement('li');
-    listItem.textContent = channel.name;
-    listItem.addEventListener('click', function () {
-      player.source = {
-        type: 'video',
-        sources: [
-          {
-            src: channel.url,
-            type: 'application/x-mpegURL'
-          }
-        ]
-      };
+  function displayChannels(channels) {
+    channelList.innerHTML = '';
+    channels.forEach(channel => {
+      const listItem = document.createElement('li');
+      listItem.textContent = channel.name;
+      listItem.addEventListener('click', function () {
+        player.source = {
+          type: 'video',
+          sources: [
+            {
+              src: channel.url,
+              type: 'application/x-mpegURL'
+            }
+          ]
+        };
+      });
+      channelList.appendChild(listItem);
     });
-    channelList.appendChild(listItem);
+  }
+
+  searchInput.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredChannels = channels.filter(channel => 
+      channel.name.toLowerCase().includes(searchTerm)
+    );
+    displayChannels(filteredChannels);
   });
+
+  displayChannels(channels);
 });
